f604375788ecf591a6e9ef35dadab38d3a9bec4b
krishna1608 <rishnak10@gmail.com>
2024-10-23 11:33:04 +0530

    Simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[principle,setPriniciple]=useState(0)
+ const[rate,setRate]=useState(0)
+ const[year,setYear]=useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div style={{minHeight:'100vh',width:'100%'}} className='d-flex justify-content-center align-items-center bg-primary'>
+        <div className=' box bg-info p-5 rounded'>
+          <h5 className='text-danger fw-bolder' style={{textDecoration:"underline", color:"pink"}}>Simple-Interest calculator</h5>
+          <p>Calculate your simple interest with you</p>
+          <div className='d-flex justify-content-center align-items-center p-5 rounded bg-warning'>
+            <h1 className='text-dnger'>&#8377; 0</h1>
+          </div>
+          <div className='mt-5'>
+       <form className='border rounded p-3 d-flex flex-column p-3'>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+       <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+</form>
+          </div>
+          <div className="mt-5 d-flex justify-content-between ">
+          <Button variant="text">calculate</Button>
+<Button variant="contained">Reset</Button>
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+
+
+     
+</div>
   )
 }
 
